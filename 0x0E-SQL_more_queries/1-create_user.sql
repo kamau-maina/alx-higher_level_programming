910b248e59edc4aa44f1da39d62d3e491ddefb49
kamau-maina <mainkamjames@gmail.com>
2023-02-17 05:46:56 +0300

    added comment

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
-reates the user user_0d_1 with all privileges.
+-- creates the user user_0d_1 with all privileges.
 CREATE USER
 	IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
